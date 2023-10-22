9bf5fcb1838eb3344fc6c6825540e106cd595c31
moranhu <moranhu@tcd.ie>
2023-10-22 21:51:14 +0100

    some fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd 
 
 # Problem 1
-url1 = "https://api.openvolt.com/v1/interval-data?meter_id=6514153c23e3d1424bf82738&granularity=month&start_date=2023-01-01&end_date=2023-01-31"
+url1 = "https://api.openvolt.com/v1/interval-data?meter_id=6514153c23e3d1424bf82738&granularity=month&start_date=2023-01-01&end_date=2023-02-01"
 
 headers = {
     "accept": "application/json",
@@ -20,14 +20,11 @@ consumption = consumption_data['data'][0]['consumption']
 print(consumption)
 
 # Problem 2
-url2 = "https://api.openvolt.com/v1/interval-data?meter_id=6514167223e3d1424bf82742&granularity=hh&start_date=2023-01-01&end_date=2023-02-1"
+url2 = "https://api.openvolt.com/v1/interval-data?meter_id=6514167223e3d1424bf82742&granularity=hh&start_date=2023-01-01&end_date=2023-02-01"
 
 half_hourly_consumption = pd.DataFrame(get_response(url2).json()['data'])
 
 half_hourly_consumption.drop(half_hourly_consumption.index[-1], inplace=True)
-
-
-
 
 # 14 day limit is not enforced 
 carbon_request = requests.get('https://api.carbonintensity.org.uk/intensity/2023-01-01T00:30Z/2023-02-01T00:00Z')
@@ -38,9 +35,9 @@ half_hourly_carbon = pd.DataFrame(carbon_data['data'])
 half_hourly_carbon = pd.json_normalize(half_hourly_carbon['intensity'])
 
 
-half_hourly_consumption['consumption'] = half_hourly_consumption['consumption'].astype(float)
+half_hourly_consumption['consumption'] = half_hourly_consumption['consumption'].astype(int)
 half_hourly_carbon['building_emissions'] = half_hourly_consumption['consumption'] * half_hourly_carbon['actual']
-total_carbon_emissions = half_hourly_carbon['building_emissions'].sum()
+total_carbon_emissions = half_hourly_carbon['building_emissions'].sum() / 1000
 
 print(total_carbon_emissions)
 
